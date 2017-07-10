728b490c008a8372b44998488bc201c84d3e628b
Thinh P. Tran <trphthinh@gmail.com>
2017-07-10 17:07:23 +0700

    Shopping example.

--- src/clj/learnclara/core.clj
+++ src/clj/learnclara/core.clj
@@ -1,43 +1,121 @@
 (ns learnclara.core
-  (:require [clara.rules :refer :all]))
+  (:require [clara.rules.accumulators :as acc]
+            [clara.rules :refer :all]))
 
-(defrecord SupportRequest [client level])
-(defrecord ClientRepresentative [name client])
+;;;; Facts used in the examples below
 
-(defrule is-important
-  "Find important support requests."
-  [SupportRequest (= :high level)]
+(defrecord Order [year month day])
+(defrecord Customer [status])
+(defrecord Purchase [cost item])
+(defrecord Discount [reason percent])
+(defrecord Total [total])
+(defrecord Promotion [reason type])
+
+;;;; Some example rules.
+
+(defrule total-purchases
+  [?total <- (acc/sum :cost) :from [Purchase]]
   =>
-  (println "High support request!"))
+  (insert! (->Total ?total)))
 
-(defrule notify-client-rep
-  "Find the client representativ and send a notification of a support request."
-  [SupportRequest (= ?client client)]
-  [ClientRepresentative (= ?client client) (= ?name name)] ;Join via the ?client binding
+;;; Discount
+(defrule summer-special
+  "Place an order in the summer and get 20% off!"
+  [Order (#{:june :july :august} month)]
   =>
-  (println "Notify" ?name "that" ?client "has a new support request!"))
+  (insert! (->Discount :summer-special 20)))
 
-(defrule notify-client-rep-on-high
-  "Find the client representativ and send a notification of a high level request."
-  [SupportRequest (= ?client client) (= :high level)]
-  [ClientRepresentative (= ?client client) (= ?name name)]
+(defrule vip-discount
+  "VIPs get a discount on purchases over $100. Cannot be combined with any other discount."
+  [Customer (= status :vip)]
+  [Total (> total 100)]
   =>
-  (println "Nofify" ?name "that" ?client "has a high level request!"))
+  (insert! (->Discount :vip 10)))
 
-(defrule notify-client-rep-on-low
-  "Find the client representativ and send a notification of a low level request."
-  [SupportRequest (= ?client client) (= :low level)]
-  [ClientRepresentative (= ?client client) (= ?name name)]
+(def max-discount
+  "Accummulator that returns the highest percentage discount."
+  (acc/max :percent :returns-fact true))
+
+(defquery get-best-discount
+  "Query to find the best discount that can be applied"
+  []
+  [?discount <- max-discount :from [Discount]])
+
+;;; Promotions.
+(defrule free-widget-month
+  "All purchases over $200 in August get a free widget."
+  [Order (= :august month)]
+  [Total (> total 200)]
   =>
-  (println "Nofify" ?name "that" ?client "has a low level request!"))
+  (insert! (->Promotion :free-widget-month :widget)))
 
-;; Run the rules! We can just use Clojure's threading macro to wire thing up.
-(-> (mk-session)
-    (insert (->ClientRepresentative "Alice" "Acme")
-            (->SupportRequest "Acme" :high)
-            (->ClientRepresentative "Thinh" "Appsmith")
-            (->SupportRequest "Appsmith" :low))
-    (fire-rules))
+(defrule free-lunch-with-gizmo
+  "Anyone who purchases a gizmo gets a free lunch."
+  [Purchase (= item :gizmo)]
+  =>
+  (insert! (->Promotion :free-lunch-with-gizmo :lunch)))
+
+(defquery get-promotions
+  "Query to find promotions for the purchase."
+  []
+  [?promotion <- Promotion])
+
+;;;; The section below shows this example in action. ;;;;
+
+(defn print-discounts!
+  "Print the discounts from the given session."
+  [session]
+
+  ;; Destructure and print each discount.
+  (doseq [{{reason :reason percent :percent} :?discount} (query session get-best-discount)]
+    (println percent "%" reason "discount???"))
+
+  session)
+
+(defn print-promotions!
+  "Prints promotions from the given session"
+  [session]
+
+  (doseq [{{reason :reason type :type} :?promotion} (query session get-promotions)]
+    (println "Free" type "for promotion" reason))
+
+  session)
+
+(defn run-examples
+  "Function to run the above example."
+  []
+  (println "VIP shopping example:")
+  ;; prints "10 % :vip discount"
+  (-> (mk-session) ; Load the rules.
+      (insert (->Customer :vip)
+              (->Order 2013 :march 20)
+              (->Purchase 20 :gizmo)
+              (->Purchase 120 :widget)) ; Insert some facts.
+      (fire-rules)
+      (print-discounts!))
+
+  (println "Summer special and widget promotion example:")
+  ;; prints: "20 % :summer-special discount"
+  ;;         "Free :lunch for promotion :free-lunch-for-gizmo"
+  ;;         "Free :widget for promotion :free-widget-month"
+  (-> (mk-session) ; Load the rules.
+      (insert (->Customer :vip)
+              (->Order 2013 :august 20)
+              (->Purchase 20 :gizmo)
+              (->Purchase 120 :widget)
+              (->Purchase 90 :widget)) ; Insert some facts.
+      (fire-rules)
+      (print-discounts!)
+      (print-promotions!))
+nil)
+
+
+
+
+
+
+
+
 
 
 
